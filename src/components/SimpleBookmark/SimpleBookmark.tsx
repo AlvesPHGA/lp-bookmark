16996c90bbef3fbca6c1a7334a5e7d51c285be75
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-01-29 11:46:34 -0300

    created a new component to section one of page with name SimpleBookmark

---
+++ src/components/SimpleBookmark/SimpleBookmark.tsx
@@ -0,0 +1,11 @@
+export function SimpleBookmark() {
+   return (
+      <section>
+         <div>
+            <h1></h1>
+            <p></p>
+            <div></div>
+         </div>
+      </section>
+   );
+}

474c540a6948f12e1f27f5a53f342fd4961ac9b2
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-01-30 12:23:36 -0300

    updated SimpleBookmark

---
+++ src/components/CTA/CTA.tsx
@@ -0,0 +1,7 @@
+export function CTA({ legend }) {
+   return (
+      <a href="" className="block border-2 w-fit py-3 px-6 rounded text-base">
+         Get it on {legend}
+      </a>
+   );
+}
--- src/components/SimpleBookmark/SimpleBookmark.tsx
+++ src/components/SimpleBookmark/SimpleBookmark.tsx
@@ -1,3 +1,7 @@
+import { CTA } from '../CTA/CTA';
+
+const cta = ['Chrome', 'Firefox'];
+
 export interface ISimpleBM {
    title: string;
    text: string;
@@ -8,8 +12,12 @@ export function SimpleBookmark({ title, text }: ISimpleBM) {
          <div className="w-[1200px] mx-auto">
             <div className="w-[540px]">
                <h1 className="text-5xl font-medium">{title}</h1>
-               <p className="text-grayishBlue my-5">{text}</p>
-               <div></div>
+               <p className="text-grayishBlue my-7">{text}</p>
+               <div className="flex gap-3">
+                  {cta.map((c) => (
+                     <CTA key={c} legend={c} />
+                  ))}
+               </div>
             </div>
          </div>
       </section>

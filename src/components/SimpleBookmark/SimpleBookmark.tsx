5610a3047e5ed7c8e3278bfc185f808ffdfd75e7
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-01-30 12:09:53 -0300

    updated SimpleBookmark component: was added content

--- src/components/SimpleBookmark/SimpleBookmark.tsx
+++ src/components/SimpleBookmark/SimpleBookmark.tsx
@@ -1,10 +1,16 @@
-export function SimpleBookmark() {
+export interface ISimpleBM {
+   title: string;
+   text: string;
+}
+export function SimpleBookmark({ title, text }: ISimpleBM) {
    return (
-      <section>
-         <div>
-            <h1></h1>
-            <p></p>
-            <div></div>
+      <section className="w-full py-20">
+         <div className="w-[1200px] mx-auto">
+            <div className="w-[540px]">
+               <h1 className="text-5xl font-medium">{title}</h1>
+               <p className="text-grayishBlue my-5">{text}</p>
+               <div></div>
+            </div>
          </div>
       </section>
    );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import { SimpleBookmark } from '../components/SimpleBookmark/SimpleBookmark';
 
+import { SimpleBookmarkManager as SBM } from '../../content.mock.json';
+
 export function Home() {
+   console.log(SBM);
    return (
       <>
-         <SimpleBookmark />
+         <SimpleBookmark {...SBM} />
       </>
    );
 }

52066763ab7dfbe43cc53c776ce616a89102b10e
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-01 18:42:47 -0300

    created a new component with name Tab to Feature section, it's a content of section

---
+++ src/components/Feature/Tab.tsx
@@ -0,0 +1,30 @@
+export function Tab() {
+   return (
+      <div className="pt-5 w-full border">
+         <div className="w-fit mx-auto border-b border-b-grayishBlue">
+            <button className="text-lg border px-10 py-3">
+               Simple Bookmark
+            </button>
+            <button className="px-10 text-lg">Speedy Searching</button>
+            <button className="text-lg px-10">Easy Sharing</button>
+         </div>
+         <div className="mt-14 flex items-center justify-between">
+            <div className="w-[600px] h-[400px] bg-[url(./src/assets/illustration-features-tab-1.svg)] bg-cover bg-center rounded-lg"></div>
+            <div className="w-[500px]">
+               <h3 className="text-3xl font-medium">Bookmark in one click</h3>
+               <p className="my-8">
+                  Organize your bookmarks however you like. Our simple
+                  drag-and-drop interface gives you complete control over how
+                  you manage your favorite sites.
+               </p>
+               <a
+                  href=""
+                  className="block w-fit px-5 py-2 text-base rounded bg-[#5368DF] text-white border-2 border-[#5368DF] hover:text-[#5368DF] hover:bg-transparent transition-all"
+               >
+                  More info
+               </a>
+            </div>
+         </div>
+      </div>
+   );
+}

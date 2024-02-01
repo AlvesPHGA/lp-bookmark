7648c8298154dfa7ee23b5592913135209895c61
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-01 11:56:27 -0300

    updated proj: created a new component to Features section, with name Feature

---
+++ src/components/Feature/Feature.tsx
@@ -0,0 +1,10 @@
+export function Features() {
+   return (
+      <section className="py-10">
+         <div>
+            <h2></h2>
+            <p></p>
+         </div>
+      </section>
+   );
+}

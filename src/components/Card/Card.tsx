74f18df6fb63b2c1cb273e4b7813ef7717aa21e7
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-06 12:03:44 -0300

    updated cards layout

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,15 +6,20 @@ export interface ICard {
 
 export function Card({ image, title, version }: ICard) {
    return (
-      <div>
-         <img src={image} alt="" />
-         <div>
-            <h3>{title}</h3>
-            <span>Minimum version {version}</span>
+      <div className="relative p-5 pt-10 rounded-xl shadow-lg">
+         <img className="mx-auto" src={image} alt="" />
+         <div className="text-center mt-5 mb-12">
+            <h3 className="text-xl font-medium mb-1">{title}</h3>
+            <span className="text-sm text-grayishBlue">
+               Minimum version {version}
+            </span>
          </div>
          <div>
-            <button>Add & Install Extesion</button>
+            <button className="border-2 border-[#5368DF] px-6 py-2 rounded text-base bg-[#5368DF] text-white hover:bg-transparent hover:text-[#5368DF] transition-all">
+               Add & Install Extesion
+            </button>
          </div>
+         <div className="absolute w-full h-1 bottom-[5rem] bg-[url('./src/assets/bg-dots.svg')] left-0"></div>
       </div>
    );
 }

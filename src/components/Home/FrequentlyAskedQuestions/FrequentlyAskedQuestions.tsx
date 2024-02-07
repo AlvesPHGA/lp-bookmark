81e4ed9711314a98e2f94cd0c920c4a14bcf9e28
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-07 12:02:16 -0300

    created a new component with name FrequentlyAskedQuestion to FrequentlyAskedQuestion section

---
+++ src/components/Home/FrequentlyAskedQuestions/FrequentlyAskedQuestions.tsx
@@ -0,0 +1,12 @@
+export function FrequentlyAskedQuestions() {
+   return (
+      <section>
+         <div className="w-[1200px] mx-auto">
+            <div>
+               <h2></h2>
+               <p></p>
+            </div>
+         </div>
+      </section>
+   );
+}

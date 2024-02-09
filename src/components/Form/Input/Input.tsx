edee784c7ad7283ad4257fac0ba596e4744d49c5
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-09 12:17:36 -0300

    updated layout of Input component

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -1,8 +1,15 @@
 export function Input() {
    return (
-      <div>
-         <input type="email" name="email" id="email" />
-         <button>Contact Us</button>
+      <div className="w-[40%] mx-auto mt-10 flex justify-between">
+         <input
+            className="w-[68%] py-[2px] px-2 rounded-md"
+            type="email"
+            name="email"
+            id="email"
+         />
+         <button className="w-fit px-5 py-2 bg-softRed text-white text-base rounded-md">
+            Contact Us
+         </button>
       </div>
    );
 }

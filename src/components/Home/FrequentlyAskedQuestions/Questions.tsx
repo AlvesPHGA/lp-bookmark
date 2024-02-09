02dc1eda7ea709f4fb43173a821be88c8720fd22
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-09 11:38:54 -0300

    added hover action on title of questions

--- src/components/Home/FrequentlyAskedQuestions/Questions.tsx
+++ src/components/Home/FrequentlyAskedQuestions/Questions.tsx
@@ -10,7 +10,7 @@ export interface IQuestion {
 export function QuestionsList({ question, reply }: IQuestion) {
    return (
       <div className="border-t last:border-b border-grayishBlue py-5 pr-2">
-         <dt className="mb-5 text-xl text-veryDarkBlue flex justify-between items-center">
+         <dt className="mb-5 text-xl text-veryDarkBlue flex justify-between items-center cursor-pointer hover:text-softRed transition-all">
             {question}
             <ReactSVG src={ArrowDown} />
          </dt>

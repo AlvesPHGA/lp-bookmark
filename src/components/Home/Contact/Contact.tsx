07e53ceafca3d461d683c53c1d1d75f4d9b633e1
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-09 12:06:38 -0300

    added content in Contact section

--- src/components/Home/Contact/Contact.tsx
+++ src/components/Home/Contact/Contact.tsx
@@ -1,10 +1,17 @@
-export function Contact() {
+export interface IContact {
+   sutitle: string;
+   title: string;
+}
+
+export function Contact({ sutitle, title }: IContact) {
    return (
       <section className="bg-[#5368DF] py-20">
-         <div className="w-[1200px] mr-auto">
-            <div className="text-center">
-               <p></p>
-               <h2></h2>
+         <div className="w-[1200px] mx-auto">
+            <div className="text-center w-[40%] mx-auto">
+               <p className="uppercase text-white text-xs tracking-[.4rem]">
+                  {sutitle}
+               </p>
+               <h2 className="text-white mt-5">{title}</h2>
             </div>
          </div>
       </section>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,13 @@ import {
    Features as Feature,
    Download as Down,
    FrequentlyAskedQuestion as FAQ,
+   Contact as Contacts,
 } from '../../content.mock.json';
 
 import { Features } from '../components/Feature/Feature';
 import { Download } from '../components/Home/Download/Download';
 import { FrequentlyAskedQuestions } from '../components/Home/FrequentlyAskedQuestions/FrequentlyAskedQuestions';
+import { Contact } from '../components/Home/Contact/Contact';
 
 export interface IDefault {
    title: string;
@@ -23,6 +25,7 @@ export function Home() {
          <Features {...Feature} />
          <Download {...Down} />
          <FrequentlyAskedQuestions {...FAQ} />
+         <Contact {...Contacts} />
       </div>
    );
 }

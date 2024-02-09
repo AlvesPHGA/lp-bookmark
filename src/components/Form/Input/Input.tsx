2b8a2651cc7c1dc23c1aad23a92f9142c076480a
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-09 18:29:48 -0300

    updated input component layout

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -2,12 +2,13 @@ export function Input() {
    return (
       <div className="w-[40%] mx-auto mt-10 flex justify-between">
          <input
-            className="w-[68%] py-[2px] px-2 rounded-md"
+            className="w-[68%] py-[2px] px-2 rounded-md text-base"
             type="email"
             name="email"
             id="email"
+            placeholder="Enter your email address"
          />
-         <button className="w-fit px-5 py-2 bg-softRed text-white text-base rounded-md">
+         <button className="w-fit px-5 py-2 bg-softRed text-white text-base rounded-md border-2 border-softRed hover:bg-white hover:text-softRed transition-all">
             Contact Us
          </button>
       </div>

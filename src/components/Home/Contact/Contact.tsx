cd27c928de0c97883e719c2acfc3dc1502e84a7a
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-09 11:50:04 -0300

    created a new component with name Contact to Contact section

---
+++ src/components/Home/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+export function Contact() {
+   return (
+      <section className="bg-[#5368DF] py-20">
+         <div className="w-[1200px] mr-auto">
+            <div className="text-center">
+               <p></p>
+               <h2></h2>
+            </div>
+         </div>
+      </section>
+   );
+}

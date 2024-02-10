2d6420a1eb77db202cca1fd6af99804370cb1e37
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-10 12:28:54 -0300

    updated components css

--- src/components/Feature/BackBlue.tsx
+++ src/components/Feature/BackBlue.tsx
@@ -1,5 +1,3 @@
 export function Backblue() {
-   return (
-      <div className="absolute w-[900px] h-[400px] left-0 rounded-br-[184px] border border-red bottom-0 bg-[#5368DF] z-0"></div>
-   );
+   return <div className="backBlue backBlue_left"></div>;
 }
--- src/components/SimpleBookmark/BackBlue.tsx
+++ src/components/SimpleBookmark/BackBlue.tsx
@@ -1,5 +1,3 @@
 export function BackBlue() {
-   return (
-      <div className="absolute w-[795px] h-[426px] right-0 rounded-bl-[184px] border border-red bottom-0 bg-[#5368DF]"></div>
-   );
+   return <div className="backBlue backBlue_right"></div>;
 }

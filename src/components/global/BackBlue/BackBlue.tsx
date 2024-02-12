3aabffbb6adaad4ec95f7a60fc6050e1ad9e1f63
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-12 11:46:10 -0300

    updated BackBlue component

--- src/components/global/BackBlue/BackBlue.tsx
+++ src/components/global/BackBlue/BackBlue.tsx
@@ -1,12 +1,10 @@
 export function BackBlue() {
-   function position() {
-      const element = document.querySelectorAll('.backBlue');
+   const element = document.querySelectorAll('.backBlue');
+   element.forEach((item) => {
+      item.parentElement?.classList.contains('__features')
+         ? item.classList.add('backBlue_left')
+         : item.classList.add('backBlue_right');
+   });
 
-      element.forEach((item) => {
-         item.parentElement?.classList.contains('__features')
-            ? item.classList.add('backBlue_left')
-            : item.classList.add('backBlue_right');
-      });
-   }
-   return <div className={`backBlue ${position()}`}></div>;
+   return <div className={`backBlue`}></div>;
 }

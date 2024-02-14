71d9a4317245040ed1aa9f8bc4154385d0e9015d
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-14 12:22:47 -0300

    new features: was created a new component to content tabs with name ContentTab

--- src/components/Home/Feature/Tab.tsx
+++ src/components/Home/Feature/Tab.tsx
@@ -1,5 +1,7 @@
 // import { IDefault } from '../../../pages/Home';
 
+import { ContentTab } from './ContentTab';
+
 const titleTab = ['Simple Bookmark', 'Speedy Searching', 'Easy Sharing'];
 
 // export interface ITab {
@@ -20,23 +22,7 @@ export function Tab() {
                </button>
             ))}
          </div>
-         <div className="mt-14 flex items-center justify-between">
-            <div className="w-[600px] h-[400px] bg-[url(./src/assets/illustration-features-tab-1.svg)] bg-cover bg-center rounded-lg"></div>
-            <div className="w-[500px]">
-               <h3 className="text-3xl font-medium">Bookmark in one click</h3>
-               <p className="my-8">
-                  Organize your bookmarks however you like. Our simple
-                  drag-and-drop interface gives you complete control over how
-                  you manage your favorite sites.
-               </p>
-               <a
-                  href=""
-                  className="block w-fit px-5 py-2 text-base rounded bg-[#5368DF] text-white border-2 border-[#5368DF] hover:text-[#5368DF] hover:bg-transparent transition-all"
-               >
-                  More info
-               </a>
-            </div>
-         </div>
+         <ContentTab />
       </div>
    );
 }

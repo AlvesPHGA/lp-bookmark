09d564cb421d571be8c466ebcff89c1064d41224
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-03-02 19:33:59 -0300

    updated: layout of tabButtons

--- src/components/Home/Feature/TabButton.tsx
+++ src/components/Home/Feature/TabButton.tsx
@@ -8,8 +8,10 @@ export function TabButton({ legend, onClick, isActive }: TabButtonProps) {
    return (
       <button
          onClick={onClick}
-         className={`text-grayishBlue text-lg px-10 py-3 cursor-pointer transition-all hover:text-softRed ${
-            isActive ? 'active-tab' : ''
+         className={`relative text-grayishBlue text-lg px-10 py-3 cursor-pointer transition-all hover:text-softRed ${
+            isActive
+               ? 'text-veryDarkBlue after:absolute after:border-b-4 after:w-[100%] after:border-red-500 after:left-0 after:bottom-0'
+               : ''
          }`}
       >
          {legend}

887f673de271c4ed2522379330d0db2861022946
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-03-17 15:42:12 -0300

    update: add cta component on ContentTab component

--- src/components/Home/Feature/ContentTab.tsx
+++ src/components/Home/Feature/ContentTab.tsx
@@ -1,3 +1,5 @@
+import { CTA } from '../../CTA/CTA';
+
 interface ContentTabProps {
    title: string;
    text: string;
@@ -15,12 +17,9 @@ export function ContentTab({ title, text, image, isActive }: ContentTabProps) {
             <div className="w-[500px]">
                <h3 className="text-3xl font-medium">{title}</h3>
                <p className="my-8">{text}</p>
-               <a
-                  href=""
-                  className="block w-fit px-5 py-2 text-base rounded bg-[#5368DF] text-white border-2 border-[#5368DF] hover:text-[#5368DF] hover:bg-transparent transition-all"
-               >
+               <CTA className="block w-fit px-5 py-2 text-base rounded bg-[#5368DF] text-white border-2 border-[#5368DF] hover:text-[#5368DF] hover:bg-transparent transition-all">
                   More info
-               </a>
+               </CTA>
             </div>
          </div>
       </div>

65b6afdd634b5dfd2ac28677ec1623ffbab59acd
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-03-17 15:38:37 -0300

    update: CTAs component

--- src/components/CTA/CTA.tsx
+++ src/components/CTA/CTA.tsx
@@ -1,14 +1,16 @@
-export function CTA({ legend }: { legend: string }) {
+import React from 'react';
+
+type CTAProps = React.ComponentProps<'a'> & {
+   children: string;
+   className: string;
+};
+export function CTA({ children, className }: CTAProps) {
    return (
       <a
          href=""
-         className={`block border-2 w-fit py-3 px-6 rounded text-base font-medium hover:transition-all ${
-            legend === 'Firefox'
-               ? 'bg-[#F7F7F7] text-veryDarkBlue border-[#F7F7F7] hover:bg-transparent hover:border-veryDarkBlue'
-               : 'bg-[#5368DF] text-white border-[#5368DF] hover:bg-transparent hover:text-[#5368DF]'
-         }`}
+         className={`block border-2 w-fit py-3 px-6 rounded text-base font-medium hover:transition-all ${className}`}
       >
-         Get it on {legend}
+         {children}
       </a>
    );
 }
--- src/components/Home/SimpleBookmark/SimpleBookmark.tsx
+++ src/components/Home/SimpleBookmark/SimpleBookmark.tsx
@@ -3,8 +3,6 @@ import { CTA } from '../../CTA/CTA';
 import { BackBlue } from '../../UI/BackBlue/BackBlue';
 import { BackIllustration } from './BackIllustration';
 
-const cta = ['Chrome', 'Firefox'];
-
 export function SimpleBookmark({ title, text }: IDefault) {
    return (
       <section className="__simpleBookmarkManager relative w-full py-20">
@@ -15,9 +13,12 @@ export function SimpleBookmark({ title, text }: IDefault) {
                <h1 className="text-5xl font-medium">{title}</h1>
                <p className="my-7">{text}</p>
                <div className="flex gap-3">
-                  {cta.map((c) => (
-                     <CTA key={c} legend={c} />
-                  ))}
+                  <CTA className="bg-[#5368DF] text-white border-[#5368DF] hover:bg-transparent hover:text-[#5368DF]">
+                     Get it on Chrome
+                  </CTA>
+                  <CTA className="bg-[#F7F7F7] text-veryDarkBlue border-[#F7F7F7] hover:bg-transparent hover:border-veryDarkBlue">
+                     Get it on Firefox
+                  </CTA>
                </div>
             </div>
          </div>

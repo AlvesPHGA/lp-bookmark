33e0f77c3710bd0272811d90e2040985d1f07bfa
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-03-17 15:25:25 -0300

    update: add backBlue component on SimpleBookmark section

--- src/components/Home/SimpleBookmark/SimpleBookmark.tsx
+++ src/components/Home/SimpleBookmark/SimpleBookmark.tsx
@@ -1,5 +1,6 @@
 import { IDefault } from '../../../pages/Home';
 import { CTA } from '../../CTA/CTA';
+import { BackBlue } from '../../UI/BackBlue/BackBlue';
 import { BackIllustration } from './BackIllustration';
 
 const cta = ['Chrome', 'Firefox'];
@@ -11,7 +12,7 @@ const cta = ['Chrome', 'Firefox'];
 export function SimpleBookmark({ title, text }: IDefault) {
    return (
       <section className="__simpleBookmarkManager relative w-full py-20">
-         <div className="absolute w-[795px] h-[426px] right-0 rounded-bl-[184px] bottom-0 bg-[#5368DF] z-0"></div>
+         <BackBlue className="absolute w-[795px] h-[426px] right-0 rounded-bl-[184px] bottom-0 bg-[#5368DF] z-0" />
          <BackIllustration />
          <div className={`w-[1200px] mx-auto py-20`}>
             <div className="w-[540px]">

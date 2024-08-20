09928ac6104e88b88d68723a1bb793afbe12f747
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-08-20 17:21:16 -0300

    feat: create a header component

---
+++ src/components/global/header/index.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+   return (
+      <header>
+         <div>
+            <div>
+               <nav></nav>
+               <button></button>
+            </div>
+         </div>
+      </header>
+   );
+}

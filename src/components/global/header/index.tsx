d057f84ef52568a27b51a63d8fed1a93ab616fcf
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-08-21 16:30:00 -0300

    feat: add logo on header

--- src/components/global/header/index.tsx
+++ src/components/global/header/index.tsx
@@ -1,11 +1,18 @@
 import Button from '@/components/form/button';
 import { Container } from '../componentsGlobal';
 import Nav from './nav';
+import Image from 'next/image';
 
 export default function Header() {
    return (
       <header className="py-5">
          <Container>
+            <Image
+               src="./logo-bookmark.svg"
+               alt="logo Bookmark"
+               width={230}
+               height={10}
+            />
             <div className="flex gap-7 items-center">
                <Nav />
                <Button>Login</Button>

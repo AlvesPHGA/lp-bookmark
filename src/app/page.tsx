a9dabc41bc72d9beed86763ce033e3c30c54d397
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-08-21 17:12:22 -0300

    feat: create a footer to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { Footer } from '@/components/global/footer';
 import Header from '@/components/global/header';
 import Image from 'next/image';
 
 export default function Home() {
    return (
-      <>
+      <section>
          <Header />
-      </>
+         <Footer />
+      </section>
    );
 }
--- src/components/global/componentsGlobal.tsx
+++ src/components/global/componentsGlobal.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import { container } from './componentsGlobalStyle';
+import Link from 'next/link';
 
 interface ContainerProps extends React.ComponentProps<'div'> {}
 
 export function Container({ children }: ContainerProps) {
    return <div className={container()}>{children}</div>;
+}
+
+export function Nav() {
+   return (
+      <nav className="space-x-5">
+         <Link
+            className="text-xl uppercase text-blue-950 hover:text-red-500 transition"
+            href="#"
+         >
+            Features
+         </Link>
+         <Link
+            className="text-xl uppercase text-blue-950 hover:text-red-500 transition"
+            href="#"
+         >
+            Pricing
+         </Link>
+         <Link
+            className="text-xl uppercase text-blue-950 hover:text-red-500 transition"
+            href="#"
+         >
+            Contact
+         </Link>
+      </nav>
+   );
 }

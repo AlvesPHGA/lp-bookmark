e5d00ea3e16f6d2c96b66b0e98172a548b7d5c41
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-08-30 15:41:32 -0300

    style: update style of faq

--- src/components/partials/home/faq/style.tsx
+++ src/components/partials/home/faq/style.tsx
@@ -4,9 +4,9 @@ export const faq = tv({
    slots: {
       content_box: 'space-y-6 w-5/12 mx-auto text-center',
       faq_box: 'w-5/12 mx-auto flex flex-col items-center mt-14 gap-10',
-      faq_field: 'py-5 border-t border-slate-400 last:border-b',
+      faq_field: 'pt-5 pb-1 border-t border-slate-400 last:border-b relative',
       question_faq:
-         'text-blue-950 font-medium flex items-center justify-between pr-5 cursor-pointer hover:text-red-500 transition',
+         'text-blue-950 font-medium flex items-center justify-between pr-5 cursor-pointer hover:text-red-500 z-50 relative',
       arrow: 'size-10',
       answer_faq: 'mt-5 text-slate-400',
    },
@@ -15,13 +15,13 @@ export const faq = tv({
       answer: {
          hidden: {
             arrow: 'text-blue-600',
-            answer_faq: 'hidden',
+            answer_faq: 'h-0 opacity-0',
          },
       },
       active: {
          true: {
             arrow: 'text-red-500 rotate-180',
-            answer_faq: 'block',
+            answer_faq: 'static opacity-100 h-28 transition-all',
          },
       },
    },

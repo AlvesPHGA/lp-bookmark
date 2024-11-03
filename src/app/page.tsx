d9fc74a0688bd99a33137fc43e9624d2467a313e
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-11-03 12:24:02 -0300

    feat: add metadata in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,6 @@ import { Download } from '@/components/partials/home/download';
 import { FAQ } from '@/components/partials/home/faq';
 import { Features } from '@/components/partials/home/features';
 import { IntroHome } from '@/components/partials/home/intro';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-   title: 'Bookmark',
-   description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, laboriosam.',
-};
 
 export default function Home() {
    return (
